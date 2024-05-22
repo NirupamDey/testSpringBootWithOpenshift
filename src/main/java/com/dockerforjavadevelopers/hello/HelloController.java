0f85424aa0caf0ac332bcf48bbfad3d8bbfe6349
NirupamDey <99413050+NirupamDey@users.noreply.github.com>
2024-05-22 14:32:02 +0530

    Update HelloController.java
    asda

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -1,6 +1,6 @@
 package com.dockerforjavadevelopers.hello;
 
-//import java.util.Properties;
+import java.util.Properties;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +9,8 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        //Properties p = System.getProperties();
-        //String businessName = p.getProperty("businessName")); //property to get system
+        Properties p = System.getProperties();
+        String businessName = p.getProperty("businessName")); //property to get system
         return "Hello from Nirupam to the World of Openshift with business name ";
     }
     
